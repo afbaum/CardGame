8625d8490a6cbad7e4a4aac0f76d16c07e7abb03
Amanda <amanda.baum01@gmail.com>
2020-10-18 13:24:36 -0400

    Making a Sheephead Deck

--- Deck.cs
+++ Deck.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 namespace CardGame
 {
     public class Deck
     {
-        
+
+        public List<Card> CreateDeck()
+        {
+            List<string> suit = new List<string>(new string[]{"hearts", "clubs", "diamonds", "spades"});
+            List<string> number = new List<string>(new string[]{"7", "8", "9", "10", "11", "12", "13", "1"});
+            List<Card> DeckofCards = new List<Card>{};
+
+            foreach( string s in suit)
+            {
+                foreach(string n in number)
+                {
+                    Card card = new Card(s, n);
+                    DeckofCards.Add(card);
+                }
+            }   
+            return DeckofCards;     
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CardGame
 {
@@ -6,18 +7,12 @@ namespace CardGame
     {
         static void Main(string[] args)
         {
-            Card card1 = new Card("hearts", "10");
-            card1.CardName();
-            Card card2 = new Card("hearts", "11");
-            card2.CardName();
-            Card card3 = new Card("hearts", "12");
-            card3.CardName();
-            Card card4 = new Card("hearts", "13");
-            card4.CardName();
-            Card card5 = new Card("hearts", "1");
-            card5.CardName();       
-            Card card6 = new Card("hearts", "8");
-            card6.CardName();                                          
+            Deck deck = new Deck();
+            List<Card> newDeck = deck.CreateDeck();
+            foreach (Card d in newDeck)
+            {
+                d.CardName();
+            }                                      
         }
     }
 }
